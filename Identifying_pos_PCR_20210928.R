90bfd7925b52ff0fff877b8c195cc6406f611839
Elen <elen_vink@hotmail.com>
2021-09-28 16:48:44 +0100

    20210928

--- Identifying_pos_PCR_20210928.R
+++ Identifying_pos_PCR_20210928.R
@@ -9,62 +9,62 @@ library(readxl)
 #Import data
 
 Sept7 <- read_excel("~/Viral Coinfection/PCR Results/20210907allRUTs_data_update.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept7$PCR_date <- 'Sept7'
 
 Sept8 <- read_excel("~/Viral Coinfection/PCR Results/20210908allRUTS_data_updated.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept8$PCR_date <- 'Sept8'
 
 Sept9am <- read_excel("~/Viral Coinfection/PCR Results/20210909am_allRUTs_data_updated.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept9am$PCR_date <- 'Sept9am'
 
 Sept9pm <- read_excel("~/Viral Coinfection/PCR Results/20210909pm_allRUTs_data_updated.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept9pm$PCR_date <- 'Sept9pm'
 
 Sept10am <- read_excel("~/Viral Coinfection/PCR Results/20210910am_allRUTs_data_updated.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept10am$PCR_date <- 'Sept10am'
 
 Sept10pm <- read_excel("~/Viral Coinfection/PCR Results/20210910pm_allRUTs_data_updated.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept10pm$PCR_date <- 'Sept10pm'
 
 Sept14am <- read_excel("~/Viral Coinfection/PCR Results/20210914am_allRUTs_data_all.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept14am$PCR_date <- 'Sept14am'
 
 Sept14pm <- read_excel("~/Viral Coinfection/PCR Results/20210914pm_allRUTs_data_all.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept14pm$PCR_date <- 'Sept14pm'
 
 Sept16 <- read_excel("~/Viral Coinfection/PCR Results/20210916allRUTs_data_all.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept16$PCR_date <- 'Sept16'
 
 Sept22am <- read_excel("~/Viral Coinfection/PCR Results/20210922am_allRUTs_data_all.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept22am$PCR_date <- 'Sept22am'
 
 Sept22pm <- read_excel("~/Viral Coinfection/PCR Results/20210922pm_allRUTs_data_all.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept22pm$PCR_date <- 'Sept22pm'
 
 Sept27 <- read_excel("~/Viral Coinfection/PCR Results/20210927_allRUTs_data.xls") %>% 
-  select(2,3,7)
+  select(2,3,4,7)
 
 Sept27$PCR_date <- 'Sept27'
 
@@ -73,7 +73,7 @@ Sept27$PCR_date <- 'Sept27'
 
 PCR_run <- rbind(Sept7, Sept8, Sept9am, Sept9pm, Sept10am, Sept10pm, Sept14am, Sept14pm, Sept16, Sept22am, Sept22pm, Sept27)
 
-names(PCR_run)[3] <- 'CT'
+names(PCR_run)[4] <- 'CT'
 
 #Filter for positive sample results
   
@@ -94,6 +94,9 @@ PCR_run <- PCR_run %>%
 
 PCR_run <- PCR_run %>% 
   filter(str_detect(PCR_run$`CT`, 'Undetermined') == FALSE)
+
+PCR_run <- PCR_run %>% 
+  filter(str_detect(PCR_run$`Task`, 'Neg') == FALSE)
 
 #PCR_run <- PCR_run %>% 
   #sort(as.numeric('CT', FALSE))
